2beabaef951d913b2bb6121aeff0a80c070408b7
9831011 <9831011@aut.ac.ir>
2020-03-28 17:01:34 +0430

    Add isEmpty and draw method to Table class and Change body of fullCell method with isEmpty method

--- src/Table.java
+++ src/Table.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 /**
  * Table
  *
@@ -54,10 +56,104 @@ public class Table {
      */
     public void fullCell(int color, int x, int y) {
 
-        if(color == 0) {
-            table[x][y] = 1;
-        }else if (color == 1) {
-            table[x][y] = 2;
+        if (isEmpty(x, y)) {
+            if(color == 0) {
+                table[x][y] = 1;
+            }else if (color == 1) {
+                table[x][y] = 2;
+            }
+        } else {
+            System.out.println("This cell is full!\nPlease select other cell!!!");
         }
+    }
+
+
+    private boolean isEmpty(int x, int y) {
+        if(table[x][y] == 0) {
+            return true;
+        }
+        return false;
+    }
+
+
+    public String draw(ArrayList<Disc> discs) {
+
+        // unicode of this table
+
+        String up_left = "\u2554";
+        String up_right = "\u2557";
+        String down_left = "\u255A";
+        String down_right = "\u255D";
+
+        String horizentalBar = "\u2550";
+        String verticalBar = "\u2551";
+
+        String sol = "\u1690";
+
+        String middleVerLeft = "\u2560";
+        String middleVerRight = "\u2563";
+        String middleHorUp = "\u2566";
+        String middleHorDown = "\u2569";
+        String cross = "\u256C";
+
+
+        // Result String
+
+        String res = "";
+
+
+        // Drawing Part
+
+
+        for(int i=0;i<18;i++) {
+            for(int j=0;j<9;j++) {
+
+                if (i==1 && j==1) {
+                    res += up_left+horizentalBar+sol+sol+sol+horizentalBar+middleHorUp;
+                } else if (i==1 && j==8) {
+                    res += horizentalBar+sol+sol+sol+horizentalBar+up_right;
+                } else if (i==17 && j==1) {
+                    res += down_left+horizentalBar+sol+sol+sol+horizentalBar+middleHorDown;
+                } else if (i==17 && j==8) {
+                    res += horizentalBar+sol+sol+sol+horizentalBar+down_right;
+                } else if (j==1 && i%2==1) {
+                    res += middleVerLeft+horizentalBar+sol+sol+sol+horizentalBar+cross;
+                } else if (j==8 && i%2==1) {
+                    res += horizentalBar+sol+sol+sol+horizentalBar+middleVerRight;
+                } else if (j==0 && i%2==0 && i!=0) {
+                    res += i/2;
+                } else if (j==0 && i%2==1) {
+                    res += " ";
+                } else if (i==0) {
+                    char letter = 'A';
+                    if (j==1) {
+                        res += "   "+letter+"   ";
+                    } else if (j>1) {
+                        letter+=j-1;
+                        res += " "+letter+"  ";
+                        if (letter=='D') {
+                            res += " ";
+                        } else if (letter=='G') {
+                            res += " ";
+                        }
+                    }
+                } else if (i==1) {
+                    res += horizentalBar+sol+sol+sol+horizentalBar+middleHorUp;
+                } else if (i==17) {
+                    res += horizentalBar+sol+sol+sol+horizentalBar+middleHorDown;
+                } else if(i%2==1) {
+                    res += horizentalBar+sol+sol+sol+horizentalBar+cross;
+                } else { //check for player disc
+                    if (j==1) {
+                        res += verticalBar+" "+sol+sol+sol+" "+verticalBar;
+                    } else if (j>1) {
+                        res += " "+sol+sol+sol+" "+verticalBar;
+                    }
+                }
+            }
+            res += "\n";
+        }
+
+        return res;
     }
 }
