3ed5b50e734387dffd491d3e93a34cab9662219d
9831011 <9831011@aut.ac.ir>
2020-03-28 17:09:10 +0430

    Create Condition class

---
+++ src/Condition.java
@@ -0,0 +1,11 @@
+/**
+ * Condition
+ *
+ * This class has method to check conditions of
+ * this game and check end of game
+ *
+ * @author Amir01
+ * @version v1.0 (27 Mar 2020)
+ */
+public class Condition {
+}
