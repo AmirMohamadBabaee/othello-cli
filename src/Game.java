f5a1e92355e883bc806baab29c892bf89eaaef67
9831011 <9831011@aut.ac.ir>
2020-03-28 18:40:00 +0430

    Create Game class

---
+++ src/Game.java
@@ -0,0 +1,12 @@
+/**
+ * Game
+ *
+ * This class manage all of the condition and elements of
+ * this game. all of the element there is in this game such as
+ * list of discs in this game.
+ *
+ * @author Amir01
+ * @version v1.0 (28 Mar 2020)
+ */
+public class Game {
+}
